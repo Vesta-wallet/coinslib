ecbd491c90e919babe70eaff8937fa84fed2a033
peerchemist <dx.1178@gmail.com>
2021-12-03 17:16:13 +0100

    Update check_types.dart
    Remove this silly thing.

--- lib/src/utils/check_types.dart
+++ lib/src/utils/check_types.dart
@@ -1,12 +1,6 @@
 import 'dart:typed_data';
 import 'dart:math';
 
-const SATOSHI_MAX = 21 * 1e14;
-
-bool isShatoshi(int value) {
-  return isUint(value, 53) && value <= SATOSHI_MAX;
-}
-
 bool isUint(int value, int bit) {
   return (value >= 0 && value <= pow(2, bit) - 1);
 }
